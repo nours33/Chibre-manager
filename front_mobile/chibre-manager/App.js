609e9a9c325ee20d3abc5c31b386dc069a93224f
Teo Sesti <teo.sesti@wavemind.ch>
2021-03-02 12:14:55 +0100

    Mise a jour de la Planification

--- front_mobile/chibre-manager/App.js
+++ front_mobile/chibre-manager/App.js
@@ -1,63 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
-import { Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-const {width, height} = Dimensions.get('window')
+import HomeScreen from "./src/screens/Home/HomeScreen";
+import CreateGameScreen from "./src/screens/CreateGame/CreateGameScreen";
+import GameScreen from "./src/screens/Game/GameScreen";
 
 export default function App() {
-
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Chibre Manager</Text>
-      </View>
-      <View style={styles.imgContainer}>
-        <Image source={require('../assets/img/logo_home.jpg')}/>
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.buttonSingleContainer}>
-          <Button mode="contained">Commencer une partie</Button>
-        </View>
-        <View>
-          <Button mode="contained">Reprendre une partie</Button>
-        </View>
-      </View>
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        // screenOptions={{
+        //   headerShown: false
+        // }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Création de la partie" component={CreateGameScreen} />
+        <Stack.Screen name="commencez la partie" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-around'
-  },
-  titleContainer: {
-    marginTop: width/3,
-  },
-  title: {
-    fontSize: 42,
-  },
-  buttonContainer: {
-    flex: 1,
-    maxHeight: 200,
-    justifyContent: 'flex-start',
 
-  },
-  buttonSingleContainer: {
-    marginBottom: 30,
-  },
-  button: {
-    paddingTop: 10
-  },
-  imgContainer: {
-    flex: 1,
-    justifyContent: 'center',
-
-  },
-  img: {
-
-  }
-});
