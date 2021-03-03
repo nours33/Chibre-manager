f70e776ded0c41a8814a3110f498f676918d104b
Teo Sesti <teo.sesti@wavemind.ch>
2021-03-03 21:29:29 +0100

    Nouveau composant pour l'affichage des historique des matches

---
+++ front_mobile/chibre-manager/src/components/game-history/index.js
@@ -0,0 +1,58 @@
+/**
+ * The external dependencies
+ */
+import React, {useState, useEffect} from 'react';
+import {Image, Text, View, FlatList,TouchableOpacity} from "react-native";
+
+/**
+ * The Internal dependencies
+ */
+import { DataTable } from 'react-native-paper';
+import moment from 'moment'
+
+
+
+const GameHistory = (props) => {
+  return (
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Nom</DataTable.Title>
+        <DataTable.Title>Team</DataTable.Title>
+        <DataTable.Title>Joueur</DataTable.Title>
+        <DataTable.Title>Date</DataTable.Title>
+      </DataTable.Header>
+
+        <FlatList
+          keyExtractor={(post) => post.id.toString()}
+          data={props['GameData']}
+          renderItem={({item}) => {
+
+           var date_good_format = moment(item.created_at).local('fr').format('L')
+
+            console.log(item)
+
+            if (item.status && item['teams'][0] !== undefined ) {
+              if ( item['teams'][0].player[0] !== undefined ) {
+
+                console.log(item['teams'][0].player)
+                return (
+                  <TouchableOpacity>
+                    <DataTable.Row>
+                      <DataTable.Cell>{item.name}</DataTable.Cell>
+                      <DataTable.Cell>{item['teams'][0].name}  {item['teams'][1].name}</DataTable.Cell>
+                      <DataTable.Cell>{item['teams'][0].player[0].name} {item['teams'][0].player[1].name} {item['teams'][1].player[0].name} {item['teams'][1].player[1].name}</DataTable.Cell>
+                      <DataTable.Cell>{date_good_format}</DataTable.Cell>
+                    </DataTable.Row>
+                  </TouchableOpacity>
+                )
+              }
+            }
+          }}
+
+        />
+    </DataTable>
+  )
+
+}
+
+export default GameHistory;
