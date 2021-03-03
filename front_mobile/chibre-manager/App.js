fa5cb9e2a3260253a4f8e8573e9931c6148b9e87
Teo Sesti <teo.sesti@wavemind.ch>
2021-03-03 11:17:45 +0100

    Update App

--- front_mobile/chibre-manager/App.js
+++ front_mobile/chibre-manager/App.js
@@ -1,11 +1,18 @@
+/**
+ * The external dependencies
+ */
 import React from 'react';
+
+/**
+ * The Internal dependencies
+ */
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-
 import HomeScreen from "./src/screens/Home/HomeScreen";
-import CreateGameScreen from "./src/screens/CreateGame/CreateGameScreen";
+import CreateFirstTeam from "./src/screens/CreateFirstTeam/CreateFirstTeamScreen.js";
+import CreateSecondTeamScreen from "./src/screens/CreateSecondTeam/CreateSecondTeamScreen";
 import GameScreen from "./src/screens/Game/GameScreen";
+
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -17,8 +24,9 @@ export default function App() {
         // }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Création de la partie" component={CreateGameScreen} />
-        <Stack.Screen name="commencez la partie" component={GameScreen} />
+        <Stack.Screen name="CreateTeam1" component={CreateFirstTeam} />
+        <Stack.Screen name="CreateTeam2" component={CreateSecondTeamScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
