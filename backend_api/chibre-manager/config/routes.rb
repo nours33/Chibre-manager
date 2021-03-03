25604b3b15b03a297aa4622815f8786bc2426349
Teo Sesti <teo.sesti@wavemind.ch>
2021-03-03 21:27:23 +0100

    Mise a jour des routes

--- backend_api/chibre-manager/config/routes.rb
+++ backend_api/chibre-manager/config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :announces, only: [:index]
       resources :games, only: [:index, :create]
-      resources :teams, only: [:index, :create] do
-        resources :players, only: [:index, :create]
-      end
+      resources :teams, only: [:index, :create]
+      resources :players, only: [:index, :create]
       resources :player_announces, only: [:index]
     end
   end
